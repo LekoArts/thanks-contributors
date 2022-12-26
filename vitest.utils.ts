d1781a7c84ea518097f927d086a03bdff2968573
Lennart <lekoarts@gmail.com>
2022-12-26 16:58:57 +0100

    chore: Dependency updates (#4)

--- vitest.utils.ts
+++ vitest.utils.ts
@@ -3,51 +3,44 @@ import { join } from "path"
 import strip from "strip-ansi"
 
 type CreateLogsMatcherOutput = {
-  should: {
-    contain: (match: string) => void
-  }
+	should: {
+		contain: (match: string) => void
+	}
 }
 
 const createLogsMatcher = (output: string): CreateLogsMatcherOutput => {
-  return {
-    should: {
-      contain: match => {
-        expect(output).toMatch(new RegExp(match.replace(/\s+/g, `\\s+`)))
-      },
-    },
-  }
+	return {
+		should: {
+			contain: (match) => {
+				expect(output).toMatch(new RegExp(match.replace(/\s+/g, `\\s+`)))
+			},
+		},
+	}
 }
 
-const cliBinLocation = join(
-  __dirname,
-  `bin.js`
-)
+const cliBinLocation = join(__dirname, `bin.js`)
 
 type InvokeCliResult = {
-  exitCode: number
-  stdout: string
-  logs: CreateLogsMatcherOutput
+	exitCode: number
+	stdout: string
+	logs: CreateLogsMatcherOutput
 }
 
 export function invokeCli(args: Array<string>): InvokeCliResult {
-  try {
-    const results = execaSync(
-      process.execPath,
-      [cliBinLocation].concat(args),
-      {
-        cwd: __dirname,
-      }
-    )
-    return {
-      exitCode: results.exitCode,
-      stdout: strip(results.stdout.toString()),
-      logs: createLogsMatcher(strip(results.stdout.toString()))
-    }
-  } catch (err) {
-    return {
-      exitCode: err.exitCode,
-      stdout: strip(err.stdout.toString()),
-      logs: createLogsMatcher(strip(err.stdout?.toString() || ``))
-    }
-  }
+	try {
+		const results = execaSync(process.execPath, [cliBinLocation].concat(args), {
+			cwd: __dirname,
+		})
+		return {
+			exitCode: results.exitCode,
+			stdout: strip(results.stdout.toString()),
+			logs: createLogsMatcher(strip(results.stdout.toString())),
+		}
+	} catch (err) {
+		return {
+			exitCode: err.exitCode,
+			stdout: strip(err.stderr.toString()),
+			logs: createLogsMatcher(strip(err.stderr?.toString() || ``)),
+		}
+	}
 }
